5fd4f2fa7015da5ae440890e361f49dc13bb7239
Seif2001 <seifelshabshiri@aucegypt.edu>
2022-11-20 02:18:04 +0200

    used omar's tobinary to convert registers to binary

--- Register.cpp
+++ Register.cpp
@@ -239,13 +239,25 @@ public:
 		string res(ss.str());
 		return res;
 	}
-
+	string toBinary(int n)
+	{
+		string bNum = "";
+		for (int i = 31; i >= 0; i--)
+		{
+			int k = n >> i;
+			if (k & 1)
+				bNum.push_back('1');
+			else
+				bNum.push_back('0');
+		}
+		return bNum;
+	}
 	void print()
 	{
 		cout << "\nRegisters: \n";
 		for (auto it = reg.cbegin(); it != reg.cend(); ++it)
 		{
-			cout << setw(10) << left << "Register: " << setw(3) << left << it->first << setw(8) << left << " Value: "<<setw(16) << left <<it->second << setw(15) <<  " HexaDecimal Value: " << setw(15) << dectohex(it->second) << setw(15) << "Octal Value: " << setw(15) << dectooct(it->second) << "\n";
+			cout << setw(10) << left << "Register: " << setw(3) << left << it->first << setw(8) << left << " Value: "<<setw(9) << left <<it->second << setw(15) <<  " HexaDecimal Value: " << setw(10) << dectohex(it->second) << setw(15) << "Octal Value: " << setw(10) << dectooct(it->second) << setw(15) << "Binary Value: " << setw(35) << toBinary(it->second) << "\n";
 		}
 		cout << endl;
 	}
